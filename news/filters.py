4576c8aa7b4508271e09ce607ceddae5ce8f0818
Konstantin <123>
2025-02-16 00:21:48 +0900

    next

--- news/filters.py
+++ news/filters.py
@@ -6,10 +6,9 @@ from .models import Post
 
 
 class NewsFilter(FilterSet):
-    autor_id__user__username = django_filters.CharFilter(lookup_expr='icontains')
-    type = django_filters.CharFilter(field_name='type')
-    title_news = django_filters.CharFilter(lookup_expr='icontains')
-    date_time = django_filters.DateFilter(field_name="date_time", lookup_expr='gt', label="Date", widget=forms.DateInput(attrs={"type": 'date'}))
+    autor_id__user__username = django_filters.CharFilter(lookup_expr='icontains',  label="Автор")
+    title_news = django_filters.CharFilter(lookup_expr='icontains', label="Название статьи")
+    date_time = django_filters.DateFilter(field_name="date_time", lookup_expr='gt', label="Дата", widget=forms.DateInput(attrs={"type": 'date'}))
 
     class Meta:
         model = Post
